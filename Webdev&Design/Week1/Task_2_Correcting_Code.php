9a69e3f190a0c5d1da2252c5f6ebdd59a868e7fe
Jessica Camacho <jessica.fernandes.camacho@student.nhlstenden.com>
2026-02-25 10:54:46 +0100

    Update Task_2_Correcting_Code.php
    Adding the code to correct in the top as a comment

--- Webdev&Design/Week1/Task_2_Correcting_Code.php
+++ Webdev&Design/Week1/Task_2_Correcting_Code.php
@@ -1,3 +1,22 @@
+<!-- Code to correct --
+
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Find the mistakes</title>
+    </head>
+    <body>
+    <?php
+        echo <h1>Califoria Occuptional Guide</h2>”;
+        echo (“<h2>Locksmiths</h2>
+        echo “<p>A locksmith installs, services, and repairs various types
+of locks and ... .”>
+    </body>
+</html>
+
+-->
+    
 <!DOCTYPE html>
 <html>
     <head>
